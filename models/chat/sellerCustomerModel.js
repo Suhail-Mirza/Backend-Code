a157517665723ad95bae3f86742dd0e7497819f0
Suhail Mirza <mirza.suhail@gmail.com>
2025-09-09 18:01:24 +0530

    second commit

---
+++ models/chat/sellerCustomerModel.js
@@ -0,0 +1,18 @@
+const{Schema, model} = require('mongoose')
+
+const sellerCustomerSchema = new Schema({
+
+
+    myId:{
+
+        type: String,
+        required:true
+    },
+    myFriends: {
+
+        type: Array,
+        required:[]
+    }
+}, {timestamps:true})
+
+module.exports = model('seller_customers',sellerCustomerSchema)
