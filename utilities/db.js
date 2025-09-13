d7b3ecd7581e54fb39d566cbae12310566f4a07a
Suhail Mirza <mirza.suhail@gmail.com>
2025-09-14 01:22:21 +0530

    Updated import

--- utilities/db.js
+++ utilities/db.js
@@ -5,9 +5,11 @@ module.exports.dbConnect =async()=>{
     try{
         if(process.env.mode==="pro"){
             await mongoose.connect(process.env.DB_PRO_URL,{useNewURLParser : true} )
+            console.log("Production Database Connected...")
         }
         else{
              await mongoose.connect(process.env.DB_LOCAL_URL,{useNewURLParser : true} )
+             console.log("Production Database Connected...")
         }
 
     }catch(error){
